7061e6444125e2c7c3a7f641b436ba3ddbe0f699
guhaoquan <1162759071@qq.com>
2021-05-30 22:26:14 +0800

    配置登录认证

--- 01-shiro-boot/src/main/java/com/abc/springboot/controller/indexController.java
+++ 01-shiro-boot/src/main/java/com/abc/springboot/controller/indexController.java
@@ -1,6 +1,10 @@
 package com.abc.springboot.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,7 +17,39 @@ public class indexController {
     }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login(String username, String password, Model model){
+        //创建一个shiro的Subject对象，利用这个对象来完成用户的登录认证
+        Subject subject = SecurityUtils.getSubject();
+        //判断当前用户是否已经认证过，如果已经认证过着不需要认证如果没有认证过则进入if完成认证
+        if (!subject.isAuthenticated()){
+            //创建一个用户账号和密码的Token对象，并设置用户输入的账号和面
+            //这个对象将在Shiro中被获取
+            UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username,password);
+
+            try {
+                //例如账号不存在或密码错误等等，我们需要根据不同的异常类型来判断用户的登录状态并给与友好的信息提示
+                //调用login后Shiro就会自动执行我们自定义的Realm中的认证方法
+                subject.login(usernamePasswordToken);
+            }catch (UnknownAccountException e){
+                //e.printStackTrace();
+                model.addAttribute("errorMessage","帐号错误!");
+                return "login";
+            }catch (LockedAccountException e){
+                //e.printStackTrace();
+                model.addAttribute("errorMessage","帐号被锁定!");
+                return "login";
+            }catch (IncorrectCredentialsException e){
+                //e.printStackTrace();
+                model.addAttribute("errorMessage","密码错误!");
+                return "login";
+            }catch (AuthenticationException e){
+                e.printStackTrace();
+                model.addAttribute("errorMessage","认证失败!");
+                return "login";
+            }
+
+
+        }
         return "redirect:success";
     }
 
--- 01-shiro-boot/src/main/java/com/abc/springboot/realm/MyRealm.java
+++ 01-shiro-boot/src/main/java/com/abc/springboot/realm/MyRealm.java
@@ -1,23 +1,52 @@
 package com.abc.springboot.realm;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.realm.Realm;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.AuthenticatingRealm;
 
-public class MyRealm implements Realm {
-    @Override
-    public String getName() {
-        return null;
-    }
+/**
+ * 自定义的Realm用来实现用户的认证和授权
+ * 父类AuthenticatingRealm只支持用户认证(登录)
+ */
+public class MyRealm extends AuthenticatingRealm {
 
+    /**
+     * 用户认证的方法 这个方法不通手动调用shiro会自动调用
+     * @param authenticationToken   用户身份 这里存放着用户的帐号和密码
+     * @return  用户登录成功后的身份证明
+     * @throws AuthenticationException  如果认证失败shiro会抛出各种异常
+     * UnknownAccountException      帐号不存在
+     *AuthenticationException       帐号异常
+     * LockedAccountException       帐号锁定
+     * IncorrectCredentialsException    认证失败
+     * 注意
+     *      如果这些异常不够用可以自定义异常类并继承shiro认证异常父类AuthenticationException
+     */
     @Override
-    public boolean supports(AuthenticationToken authenticationToken) {
-        return false;
-    }
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        String username = token.getUsername();//获取页面中用户的帐号
+        String password =new String(token.getPassword());//获取页面中用户的密码
+        System.out.println("当前登录的用户是:" + username + "密码为:" + password);
+        /**
+         * 认证帐号
+         */
+        if (!"admin".equals(username)&&!"lisi".equals(username)){
+            throw new UnknownAccountException();//抛出帐号错误的异常
+        }
+        /**
+         * 认证帐号
+         */
+        if ("lisi".equals(username)){
+            throw new LockedAccountException();//抛出帐号错误的异常
 
-    @Override
-    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+        }
+        /**
+         * 创建密码认证对象，由shiro自动认证
+         * 参数1：数据库中的帐号
+         * 参数2：为数据中读取数据来的密码
+         * 参数3：为当前Realm的名字
+         * 如果密码认证成功则返回一个用户身份对象，如果密码认证失败shiro会抛出异常
+         */
+        return new SimpleAuthenticationInfo(username,"123456",getName());
     }
 }
